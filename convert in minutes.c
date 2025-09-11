8667080d0eaec9952c063e860e1dd2d9578f1084
KushagraRohit <kushagrarohit01@gmail.com>
2025-09-11 12:30:59 +0530

    Add files via upload

--- convert in minutes.c
+++ convert in minutes.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main (){
+int hours;
+int minutes;
+int Total_minutes;
+
+
+printf("Enter the Hours :");
+scanf("%d",&hours);
+
+
+
+
+
+}
--- grade.c
+++ grade.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int main ()
 {
-    int n,r;
-    printf
+
+    int n,r,maths;
+    printf ("Enter  maths Subject  marks\n");
+    scanf("%d",&maths);
+    printf ("Enter Subject 2 marks");
+    printf ("Enter Subject 3 marks");
+    printf ("Enter Subject 4 marks");
+    printf ("Enter Subject 5 marks");
+    scanf ("%d", &n);
+
+
+}
+
